ecde3cc9860821a328a3d7c5a4b10c2104088cf5
sodohuuuu <sodohu@outlook.com>
2026-08-22 23:59:52 +0530

    Update vps.py

--- vps.py
+++ vps.py
@@ -14,7 +14,7 @@ from __future__ import annotations
 # =============================================================================
 # EDIT ONLY THESE SETTINGS BEFORE RUNNING: python3 bot.py
 # =============================================================================
-BOT_TOKEN = "8737125210:AAHoYa6feASjr1pa6CiAc8GrCWT0VaQdihY"
+BOT_TOKEN = "8992928277:AAEKNh-3-80f-mZTTReFtEiKzMiXd0gfQhA"
 ADMIN_ID = 8502412097 # Replace 0 with your numeric Telegram user ID from @userinfobot.
 
 BOT_STATUS_NAME = "UnixNodes"
